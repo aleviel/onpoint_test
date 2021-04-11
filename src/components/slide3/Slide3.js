4266d4d664ecbb0ca72596b4ec4aae9040417a5a
Alexey R <alex666alex@mail.ru>
2021-04-11 22:48:54 +0300

    arrows for third slide

--- src/components/slide3/Slide3.js
+++ src/components/slide3/Slide3.js
@@ -23,29 +23,50 @@ export default function Slide3() {
 
             <div className={Styles.routes}>
                 <div className={CN(Styles.item1, Styles.item)}>
-                    Микрофлора <br/> кишечника
+                    <div>
+                        <span className={Styles.number}> 8</span>
+                        Микрофлора <br/> кишечника
+                    </div>
                     <div className={CN(Styles.bubble)}>
                         <img src={gut} alt="gut"/>
                     </div>
+                    <span className={CN(Styles.item1__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item1__arrow, Styles.item1__arrow_reverse, Styles.arrow)}/>
                 </div>
+
                 <div className={CN(Styles.item2, Styles.item)}>
-                    1 &beta;-клетки
+                    <div>
+                        <span className={CN(Styles.number__beta, Styles.number)}>1</span> &beta;-клетки
+                    </div>
                     <div className={CN(Styles.bubble)}>
                         <span className={Styles.number}> &beta; </span>
                     </div>
+                    <span className={CN(Styles.item2__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item2_2__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item2_3__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item2_4__arrow, Styles.arrow)}/>
                 </div>
+
                 <div className={CN(Styles.item3, Styles.item)}>
                     <div className={CN(Styles.bubble)}>
                         <img src={brain} alt="brain"/>
                     </div>
-                    Головной мозг
+                    <div>
+                        <span className={Styles.number}> 7</span> Головной мозг
+                    </div>
+                    <span className={CN(Styles.item3__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item3__arrow, Styles.item3__arrow_reverse, Styles.arrow)}/>
                 </div>
 
                 <div className={CN(Styles.item4, Styles.item)}>
-                    Нарушение иммунной <br/> регуляции/воспаление
+                    <div className={Styles.item4__abs}>
+                        <span className={Styles.number}>9</span> Нарушение иммунной <br/> регуляции/воспаление
+                    </div>
                     <div className={CN(Styles.bubble)}>
                         <img src={infection} alt="infection"/>
                     </div>
+                    <span className={CN(Styles.item4__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item4__arrow, Styles.item4__arrow_reverse, Styles.arrow)}/>
                 </div>
 
                 <div className={CN(Styles.item5, Styles.item)}>
@@ -54,24 +75,36 @@ export default function Slide3() {
                             <div>
                                 <span className={Styles.number}> 2 </span>
                             </div>
-                            инкретинового <br/> эффекта
+                            <span className={Styles.incretin}>инкретинового <br/> эффекта</span>
+                            <span className={CN(Styles.item5__arrow, Styles.arrow)}/>
                         </div>
-                        <div className={Styles.right}>
+                        <div>
                             <div>
                                 <span className={Styles.number}> 3 </span>
                             </div>
-                            Дефект &alpha;-клеток
+                            <span>Дефект &alpha;-клеток</span>
                             <br/>
-                            глюкагон
+                            <span className={Styles.glucagon}>
+                                глюкагон
+                            </span>
+                            <span className={CN(Styles.item5_1__arrow, Styles.arrow)}/>
+                            <span className={CN(Styles.item5_2__arrow, Styles.arrow)}/>
                         </div>
                     </div>
                 </div>
 
                 <div className={CN(Styles.item6, Styles.item)}>
-                    Желудок
+                    <div>
+                        <span className={Styles.number}>10</span>
+                        Желудок
+                    </div>
                     <div className={CN(Styles.bubble)}>
                         <img src={stomach} alt="stomach"/>
                     </div>
+                    <span className={Styles.amylin}>амилин</span>
+                    <span className={CN(Styles.item6__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item6_2__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item6_3__arrow, Styles.arrow)}/>
                 </div>
 
                 <div className={CN(Styles.item7, Styles.item)}>
@@ -79,36 +112,49 @@ export default function Slide3() {
                 </div>
 
                 <div className={CN(Styles.item8, Styles.item)}>
+                    <div>
+                        <span className={Styles.number}> 11 </span>
+                        Почки
+                    </div>
                     <div className={CN(Styles.bubble)}>
                         <img src={buds} alt="buds"/>
                     </div>
-                    Почки
+                    <span className={CN(Styles.item8__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item8__arrow, Styles.item8__arrow_reverse, Styles.arrow)}/>
                 </div>
 
                 <div className={CN(Styles.item9, Styles.item)}>
-                    <h3 className={Styles.item9__header} >
+                    <h3 className={Styles.item9__header}>
                         Инсулинорезистентность
                     </h3>
                     <div className={Styles.item9__inner}>
                         <div className={CN(Styles.bubble)}>
                             <img src={liver} alt="liver"/>
                         </div>
-                        6 <br/> Печень
+                        <div>
+                            <span className={Styles.number}> 6 </span> Печень
+                        </div>
                     </div>
                     <div className={Styles.item9__inner}>
                         <div className={CN(Styles.bubble)}>
                             <img src={muscle} alt="muscle"/>
                         </div>
-                        5 <br/> Мышцы
+                        <div>
+                            <span className={Styles.number}> 5 </span>Мышцы
+                        </div>
                     </div>
                     <div className={Styles.item9__inner}>
                         <div className={CN(Styles.bubble)}>
                             <img src={lipid} alt="lipid"/>
                         </div>
-                        4 <br/> Жировый клетки
+                        <div className={Styles.item9__abs}><span className={Styles.number}> 4 </span> Жировый клетки
+                        </div>
                     </div>
+                    <span className={CN(Styles.item9__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item9__arrow, Styles.item9__arrow_reverse, Styles.arrow)}/>
+                    <span className={CN(Styles.item9_1__arrow, Styles.arrow)}/>
+                    <span className={CN(Styles.item9_1__arrow, Styles.item9_1__arrow_reverse, Styles.arrow)}/>
                 </div>
-
 
             </div>
         </div>
